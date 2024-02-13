b601517fb46ca2f0c50e5f262bf4db15823d4f04
Sheikh <kenneth.sheikh@hotmail.com>
2024-02-13 12:54:45 +0100

    added examples for pageination

---
+++ script.js
@@ -0,0 +1,27 @@
+// array of items to split
+// how many pages?
+// return and array split by pages
+
+// function splitArrayIntoPages(array, itemsPerPage) {
+//   return Array.from(
+//     { length: Math.ceil(array.length / itemsPerPage) },
+//     (_, index) => array.slice(index * itemsPerPage, (index + 1) * itemsPerPage),
+//   );
+// }
+
+// // Example usage
+// const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const result = splitArrayIntoPages(items, 5);
+
+// console.log(result);
+
+// export function paginate(books, pages) {
+//   const pageinatedArray = [];
+
+//   //an array of items to split (items)
+//   const bookPerPage = books / pages;
+
+//   for (const book of books) {
+//     let tempArray = [];
+//   }
+// }
